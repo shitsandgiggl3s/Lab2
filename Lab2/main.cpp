7272e70d330643c0c449630fe632169774cfb51c
Ayah <ambigouslearner28@gmail.com>
2019-02-16 21:15:28 +0400

    Initial Commit

---
+++ Lab2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab2
+//
+//  Created by Ayah Al-harthy on 16/02/2019.
+//  Copyright © 2019 Ayah Al-harthy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
